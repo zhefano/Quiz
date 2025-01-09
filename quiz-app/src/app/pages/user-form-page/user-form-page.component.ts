9979b6c9ae7d98e83fd0cf0faf617060d8ba12c8
Emmtus <emmy.granbom@outlook.com>
2025-01-09 10:13:44 +0100

    new pages

---
+++ quiz-app/src/app/pages/user-form-page/user-form-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-form-page',
+  imports: [],
+  templateUrl: './user-form-page.component.html',
+  styleUrl: './user-form-page.component.css'
+})
+export class UserFormPageComponent {
+
+}
