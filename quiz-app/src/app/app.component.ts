17ce84ea25f723598f6e2816fcee564fc41b2eb4
Igelkotte <jonas.toern@gmail.com>
2025-01-09 11:42:11 +0100

    add header and footer components to app layout

--- quiz-app/src/app/app.component.ts
+++ quiz-app/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- quiz-app/src/app/app.routes.ts
+++ quiz-app/src/app/app.routes.ts
@@ -1,14 +1,12 @@
-
-
-import { Routes } from '@angular/router';
-import { LandingPageComponent } from './pages/landing-page/landing-page.component';
-import { ResultPageComponent } from './pages/result-page/result-page.component';
-import { PlayQuizPageComponent } from './pages/play-quiz-page/play-quiz-page.component';
-import { UserFormPageComponent } from './pages/user-form-page/user-form-page.component';
+import { Routes } from "@angular/router";
+import { LandingPageComponent } from "./pages/landing-page/landing-page.component";
+import { ResultPageComponent } from "./pages/result-page/result-page.component";
+import { PlayQuizPageComponent } from "./pages/play-quiz-page/play-quiz-page.component";
+import { UserFormPageComponent } from "./pages/user-form-page/user-form-page.component";
 
 export const routes: Routes = [
-    { path: '', component: LandingPageComponent },
-    { path: 'new-player', component: UserFormPageComponent }, // Change name to NewPlayerPage?
-    { path: 'result', component: ResultPageComponent },
-    { path: 'play-quiz', component: PlayQuizPageComponent },
-    ];
+	{ path: "", component: LandingPageComponent },
+	{ path: "new-player", component: UserFormPageComponent }, // Change name to NewPlayerPage?
+	{ path: "result", component: ResultPageComponent },
+	{ path: "play-quiz", component: PlayQuizPageComponent },
+];
