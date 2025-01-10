cc0567f506e0c24c914a4c60b5a8786481f42e68
Igelkotte <jonas.toern@gmail.com>
2025-01-10 10:39:51 +0100

    Moved nav links to header from landing page so they are always visible

--- quiz-app/src/app/components/header/header.component.ts
+++ quiz-app/src/app/components/header/header.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
--- quiz-app/src/app/pages/landing-page/landing-page.component.ts
+++ quiz-app/src/app/pages/landing-page/landing-page.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
