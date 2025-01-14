eff614b081f1e62f7c0b03de11213fec7bf39b63
thallberg <T.hallberg84@gmail.com>
2025-01-14 09:41:06 +0100

    Leaderboard för create quiz funkar

--- quiz-app/src/app/components/create-quiz/create-quiz.component.ts
+++ quiz-app/src/app/components/create-quiz/create-quiz.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { LeaderboardService } from '../../services/leaderboard.service';
+import { PlayerService } from '../../services/player.service';
 
 @Component({
   selector: 'app-create-quiz',
@@ -18,10 +20,20 @@ export class CreateQuizComponent {
   feedbackClass: string = '';
   quizStarted: boolean = false;
   currentQuestionIndex: number = 0;
+  score: number = 0; //Variabel som håller koll på poängen
+
+  //Hämtar in leaderboardService samt playerService
+  constructor(
+    private leaderboardService: LeaderboardService,
+    private playerService: PlayerService
+  ) {}
 
   addQuestion() {
     if (this.newQuestion.trim()) {
-      this.questions.push({ question: this.newQuestion, answer: this.newAnswer });
+      this.questions.push({
+        question: this.newQuestion,
+        answer: this.newAnswer,
+      });
       this.newQuestion = '';
       this.newAnswer = true;
     }
@@ -48,6 +60,7 @@ export class CreateQuizComponent {
       if (isTrue === correctAnswer) {
         this.feedbackMessage = 'Correct answer! Good job!';
         this.feedbackClass = 'correct-answer';
+        this.score++; // vid korrekt svar, öka poäng med 1
       } else {
         this.feedbackMessage = 'Wrong ansver! Try again.';
         this.feedbackClass = 'incorrect-answer';
@@ -57,7 +70,22 @@ export class CreateQuizComponent {
         this.currentQuestionIndex++;
       } else {
         alert('Quizet är slut! Du kan gå tillbaka till startsidan.');
+        this.saveScore(); //Anropar savescore metoden när quizet är färdigt
       }
+    }
+  }
+
+  //samma metod som play-quiz använder
+  saveScore() {
+    const player = this.playerService.getPlayerFromLocalStorage();
+    if (player) {
+      const playerName = player.username;
+      this.leaderboardService.addScore(playerName, this.score);
+      console.log(
+        `Poäng sparad för spelare: ${playerName}, Poäng: ${this.score}`
+      );
+    } else {
+      console.warn('Ingen spelare hittades i localStorage.');
     }
   }
 
@@ -79,7 +107,3 @@ export class CreateQuizComponent {
     this.quizStarted = false;
   }
 }
-
-
-
-
