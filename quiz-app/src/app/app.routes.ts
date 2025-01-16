8c40c17c19d734ecec8dc8bcbdfbfa157bd247f6
Emmtus <emmy.granbom@outlook.com>
2025-01-16 10:46:26 +0100

    fixade tillbaka routing till resultatsidan

--- quiz-app/src/app/app.routes.ts
+++ quiz-app/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from "@angular/router";
 import { LandingPageComponent } from "./pages/landing-page/landing-page.component";
+import { ResultPageComponentComponent } from "./pages/result-page-component/result-page-component.component";
 import { PlayQuizPageComponent } from "./pages/play-quiz-page/play-quiz-page.component";
 import { CreateQuizPageComponent } from "./pages/create-quiz-page/create-quiz-page.component";
 import { NewPlayerPageComponent } from "./pages/new-player-page/new-player-page.component";
@@ -7,6 +8,7 @@ import { NewPlayerPageComponent } from "./pages/new-player-page/new-player-page.
 export const routes: Routes = [
 	{ path: "", component: LandingPageComponent },
 	{ path: "new-player", component: NewPlayerPageComponent },
+	{ path: "result", component: ResultPageComponentComponent },
 	{ path: "play-quiz", component: PlayQuizPageComponent },
 	{ path: "create-quiz", component: CreateQuizPageComponent }	
 ];
---
+++ quiz-app/src/app/pages/result-page-component/result-page-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-result-page-component',
+  imports: [],
+  templateUrl: './result-page-component.component.html',
+  styleUrl: './result-page-component.component.css'
+})
+export class ResultPageComponentComponent {
+
+}
